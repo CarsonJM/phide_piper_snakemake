9ccdb245a1b51d2607da8917a6ad021901ec1010
Carson J. Miller <carsonjmiller@outlook.com>
2022-11-22 08:26:29 -0800

    Update virus lifestyle figure

--- workflow/scripts/10_virus_lifestyle_analysis.py
+++ workflow/scripts/10_virus_lifestyle_analysis.py
@@ -5,7 +5,8 @@ import plotly.express as px
 lifestyles = pd.read_csv(str(snakemake.input), sep='\t')
 
 # prepare for plotting
-lifestyles['Classification'] = lifestyles.apply(lambda x: 'Virulent' if x.Virulent > snakemake.params.bacphlip_prob else 'Temperate', axis=1)
+lifestyles['Classification'] = lifestyles.apply(lambda x: 'Virulent' if x.Virulent > snakemake.params.bacphlip_prob else 'Unknown', axis=1)
+lifestyles['Classification'] = lifestyles.apply(lambda x: 'Temperate' if x.Temperate > snakemake.params.bacphlip_prob else x.Classification, axis=1)
 lifestyles_group = lifestyles.groupby("Classification", as_index=False).count()
 lifestyles_group['proportions'] = lifestyles_group['Virulent']/sum(lifestyles_group["Virulent"])
 lifestyles_group["axis"] = "BACPHLIP"
