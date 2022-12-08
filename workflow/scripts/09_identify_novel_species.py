b15de0be3274813314f6ff0e397fa1b5971941d1
Carson J. Miller <carsonjmiller@outlook.com>
2022-12-08 11:16:55 -0800

    Updated novel species script

--- workflow/scripts/09_identify_novel_species.py
+++ workflow/scripts/09_identify_novel_species.py
@@ -18,4 +18,4 @@ ani_id95_top_qcov = ani_id95_sort.groupby('qname', as_index=False).first()
 # identify species-level hits
 ani_id95_top_qcov['novel_species'] = ani_id95_top_qcov.apply(lambda x: False if x.pid >= snakemake.params.min_ani and x.qcov >= snakemake.params.min_qcov and x.tcov >= snakemake.params.min_tcov else True, axis=1)
 
-ani_id95_top_qcov.to_csv(str(snakemake.output), index=False)
+ani_id95_top_qcov.to_csv(str(snakemake.output), sep='\t', index=False)
