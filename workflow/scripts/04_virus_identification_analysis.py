d8fde6779f13bd8238c64a739135643f652aef7a
Carson J. Miller <carsonjmiller@outlook.com>
2022-12-15 10:22:50 -0800

    Fixed virus identification analysis script

--- workflow/scripts/04_virus_identification_analysis.py
+++ workflow/scripts/04_virus_identification_analysis.py
@@ -11,13 +11,13 @@ tool_counts = pd.DataFrame()
 if snakemake.params.run_genomad:
     genomad_report = report[(report['virus_score'] >= snakemake.params.genomad_score)]
     tools.append('geNomad')
-    tool_counts[['assembly', 'geNomad']] = genomad_report.groupby(['assembly'], as_index=False).count()[['assembly' 'vls_id']]
+    tool_counts[['assembly', 'geNomad']] = genomad_report.groupby(['assembly'], as_index=False).count()[['assembly', 'vls_id']]
 
 if snakemake.params.run_external:
     external_report = report[(report['identity'] >= snakemake.params.min_mash_score) & (report['shared-hashes'] >= snakemake.params.min_mash_hashes) & (report['median-multiplicity'] >= snakemake.params.min_mash_multiplicity)]
     external_report.rename(columns={'shared-hashes':'shared_hashes', 'median-multiplicity':'median_multiplicity'}, inplace=True)
     tools.append('external')
-    tool_counts[['assembly','external']] = external_report.groupby(['assembly'], as_index=False).count()[['assembly' 'vls_id']]
+    tool_counts[['assembly','external']] = external_report.groupby(['assembly'], as_index=False).count()[['assembly', 'vls_id']]
 
 tool_counts['tools'] = tools
 id = tool_counts.melt(id_vars=['assembly'], value_vars=tools)
